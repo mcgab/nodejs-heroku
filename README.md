80b52f851aeb9862dae4b169ea10853b3db40a68
Lukasz Oleksak <dev@moderncargroup.com>
2018-05-30 22:34:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## NodeJS / Mongo example project
+
+Project can be deployed on Heroku.
+
+To run in dev enviroment run:
+- docker-compose up -d
+- npm install
+- set up database connection in .env (exmaple in .env.example)
+- npm run dev
+
+
+
---
+++ code/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+var cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser')
+require('dotenv').load();
+const app = express()
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+var port = process.env.PORT || 8080;
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use('/static', express.static('./static'))
+
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url)
+    .then(() => {
+        console.log("Successfully connected to the database");
+    }).catch(err => {
+        console.log('Could not connect to the database. Exiting now...');
+        process.exit();
+    });
+
+app.get('/', (req, res) => {
+    res.json({
+        "message": "waiting for connections"
+    });
+});
+
+// init app 
+app.listen(port, function () {
+    console.log('listening on ', port);
+})
